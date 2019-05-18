13ff20ecf2ff7f3f284b73ee8f47b7b77e80b514
Victor Guardiola <victor.guardiola@gmail.com>
2019-05-18 19:21:02 +0200

    refactoring code to try don't repeat code, and add new size

--- lib/flex_widget.dart
+++ lib/flex_widget.dart
@@ -8,106 +8,105 @@ import 'responsive.dart';
 
 // ignore: must_be_immutable
 class FlexWidget extends StatelessWidget {
+  static const int xs = 1;
+  static const int sm = 2;
+  static const int md = 3;
+  static const int lg = 4;
+  static const int xl = 4;
+
+  /// Columns offset for each grid type
+  Map<int, String> gridName = {
+    xs: 'xs',
+    sm: 'sm',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+  };
+
+  /// Columns offset for each grid type
+  Map<int, int> offsets = {
+    xs: 0,
+    sm: 0,
+    md: 0,
+    lg: 0,
+    xl: 0,
+  };
+
+  /// Columns offset for each grid type and the device is landscape
+  Map<int, int> offsetsLand = {
+    xs: 0,
+    sm: 0,
+    md: 0,
+    lg: 0,
+    xl: 0,
+  };
+
+  /// Columns counts for each grid type
+  Map<int, int> columns = {
+    xs: 12,
+    sm: 6,
+    md: 6,
+    lg: 3,
+    xl: 3,
+  };
+
+  /// Columns counts for each grid type and the device is landscape
+  Map<int, int> columnsLand = {
+    xs: 12,
+    sm: 6,
+    md: 6,
+    lg: 3,
+    xl: 3,
+  };
+
   /// Child for apply the flex Columns
   final Widget child;
-
-  /// Columns counts for less than 768
-  final double xs;
-
-  /// Columns offset for less than 768
-  final double xsOffset;
-
-  /// Columns counts for less than 768 and the device is landscape
-  double xsLand;
-
-  /// Columns offset for less than 768 and the device is landscape
-  double xsLandOffset;
-
-  /// Columns counts for more or equals to 768 pixels
-  final double sm;
-
-  /// Columns offset for more or equals to 768 pixels
-  final double smOffset;
-
-  /// Columns counts for more or equals to 768 pixels and the device is landscape
-  double smLand;
-
-  /// Columns offset for more or equals to 768 pixels and the device is landscape
-  double smLandOffset;
-
-  ///Columns counts for more or equals to 992 pixels
-  final double md;
-
-  /// Columns offset for more or equals to 992 pixels
-  final double mdOffset;
-
-  /// Columns counts for more or equals to 992 pixels and the device is landscape
-  double mdLand;
-
-  /// Columns offset for more or equals to 992 pixels and the device is landscape
-  double mdLandOffset;
-
-  ///Columns counts for more or equals to 1170 pixels
-  final double lg;
-
-  /// Columns offset for more or equals to 1170 pixels
-  final double lgOffset;
-
-  /// Columns counts for more or equals to 1170 pixels and the device is landscape
-  double lgLand;
-
-  /// Columns offset for more or equals to 1170 pixels and the device is landscape
-  double lgLandOffset;
-
   FlexWidget(
       {this.child,
-      this.xs = 12,
-      this.xsOffset = 0,
-      this.xsLand,
-      this.xsLandOffset,
-      this.sm = 6,
-      this.smOffset = 0,
-      this.smLand,
-      this.smLandOffset,
-      this.md = 6,
-      this.mdOffset = 0,
-      this.mdLand,
-      this.mdLandOffset,
-      this.lg = 3,
-      this.lgOffset = 0,
-      this.lgLand,
-      this.lgLandOffset}) {
-    if (xsLand == null) {
-      xsLand = xs;
-    }
-    if (smLand == null) {
-      smLand = sm;
-    }
-    if (mdLand == null) {
-      mdLand = md;
-    }
-    if (lgLand == null) {
-      lgLand = lg;
-    }
-
-    if (xsLandOffset == null) {
-      xsLandOffset = xsOffset;
-    }
-    if (smLandOffset == null) {
-      smLandOffset = smOffset;
-    }
-
-    if (mdLandOffset == null) {
-      mdLandOffset = mdOffset;
-    }
-    if (lgLandOffset == null) {
-      lgLandOffset = lgOffset;
-    }
+      xs = 12,
+      xsOffset = 0,
+      xsLand,
+      xsLandOffset,
+      sm = 6,
+      smOffset = 0,
+      smLand,
+      smLandOffset,
+      md = 6,
+      mdOffset = 0,
+      mdLand,
+      mdLandOffset,
+      lg = 3,
+      lgOffset = 0,
+      lgLand,
+      lgLandOffset,
+      xl = 3,
+      xlOffset = 0,
+      xlLand,
+      xlLandOffset}) {
+    columns[FlexWidget.xs] = xs;
+    columns[FlexWidget.sm] = sm;
+    columns[FlexWidget.md] = md;
+    columns[FlexWidget.lg] = lg;
+    columns[FlexWidget.xl] = xl;
+    columnsLand[FlexWidget.xs] = xsLand ?? xs;
+    columnsLand[FlexWidget.sm] = smLand ?? sm;
+    columnsLand[FlexWidget.md] = mdLand ?? md;
+    columnsLand[FlexWidget.lg] = lgLand ?? lg;
+    columnsLand[FlexWidget.xl] = xlLand ?? xl;
+    offsets[FlexWidget.xs] = xsOffset;
+    offsets[FlexWidget.sm] = smOffset;
+    offsets[FlexWidget.md] = mdOffset;
+    offsets[FlexWidget.lg] = lgOffset;
+    offsets[FlexWidget.xl] = xlOffset;
+    offsetsLand[FlexWidget.xs] = xsLandOffset ?? xsOffset;
+    offsetsLand[FlexWidget.sm] = smLandOffset ?? smOffset;
+    offsetsLand[FlexWidget.md] = mdLandOffset ?? mdOffset;
+    offsetsLand[FlexWidget.lg] = lgLandOffset ?? lgOffset;
+    offsetsLand[FlexWidget.xl] = xlLandOffset ?? xlOffset;
   }
 
   @override
   Widget build(BuildContext context) {
-    //obtain the screen size
     final size = MediaQuery.of(context).size;
     final orientation = MediaQuery.of(context).orientation;
     final settings = ResponsiveSettings.of(context);
@@ -115,96 +114,61 @@ class FlexWidget extends StatelessWidget {
       throw Exception("The FlexWidget is not into a ResponsiveRow");
     }
     final columnsCount = settings.columnsCount;
-
-    if (lg > columnsCount) {
-      throw Exception("lg is bigger than columnsCount = $lg  > $columnsCount");
-    } else if (md > columnsCount) {
-      throw Exception("md is bigger than columnsCount = $md  > $columnsCount");
-    } else if (sm > columnsCount) {
-      throw Exception("sm is bigger than columnsCount = $sm  > $columnsCount");
-    } else if (xs > columnsCount) {
-      throw Exception("xs is bigger than columnsCount = $xs  > $columnsCount");
+    final gridSizeValue = gridSize(size.width);
+    if (columns[gridSizeValue] > columnsCount) {
+      throw Exception(
+          "${gridName[gridSizeValue]} is bigger than columnsCount: ${columns[gridSizeValue]}  > $columnsCount");
     }
 
     //use the OrientationBuilder Widget
     //for change the size width, depends of the orientation
-
     final offset = _calcOffset(size, orientation, columnsCount);
-
-    final child = SizedBox(
+    Widget child = SizedBox(
       width: _calcWidth(size, orientation, columnsCount),
       child: this.child,
     );
 
     if (offset > 0.0) {
-      return Container(
-          margin: EdgeInsets.only(left: offset),
-          child: SizedBox(
-            width: _calcWidth(size, orientation, columnsCount),
-            child: this.child,
-          ));
-    } else {
-      return child;
+      child = Container(
+        margin: EdgeInsets.only(left: offset),
+        child: SizedBox(
+          width: _calcWidth(size, orientation, columnsCount),
+          child: this.child,
+        ),
+      );
     }
+
+    return child;
   }
 
   /// Calc the width in pixels, for the screen and rotation config used actually in the device
   double _calcWidth(Size size, Orientation orientation, columnsCount) {
     final width = size.width;
-    if (orientation == Orientation.portrait) {
-      final colWidth = width / columnsCount;
-      if (colWidth > 1170) {
-        return colWidth * lg;
-      } else if (width >= 992) {
-        return colWidth * md;
-      } else if (width >= 768) {
-        return colWidth * sm;
-      } else {
-        return colWidth * xs;
-      }
-    } else {
-      //Landscape configuration
-
-      final colWidth = width / columnsCount;
-      if (colWidth > 1170) {
-        return colWidth * lgLand;
-      } else if (width >= 992) {
-        return colWidth * mdLand;
-      } else if (width >= 768) {
-        return colWidth * smLand;
-      } else {
-        return colWidth * xsLand;
-      }
-    }
+    final colWidth = width / columnsCount;
+    final orientedColumns = orientation == Orientation.portrait ? columns : columnsLand;
+    return colWidth * orientedColumns[gridSize(size.width)];
   }
 
   double _calcOffset(Size size, Orientation orientation, columnsCount) {
-    if (orientation == Orientation.portrait) {
-      final width = size.width;
+    final width = orientation == Orientation.portrait ? size.width : size.height;
+    final colWidth = width / columnsCount;
+    final orientedOffsets = orientation == Orientation.portrait ? offsets : offsetsLand;
+    return colWidth * orientedOffsets[gridSize(size.width)];
+  }
 
-      final colWidth = width / columnsCount;
-      if (colWidth > 1170) {
-        return colWidth * lgOffset;
-      } else if (width >= 992) {
-        return colWidth * mdOffset;
-      } else if (width >= 768) {
-        return colWidth * smOffset;
-      } else {
-        return colWidth * xsOffset;
-      }
-    } else {
-      //Landscape configuration
-      final width = size.height;
-      final colWidth = width / columnsCount;
-      if (colWidth > 1170) {
-        return colWidth * lgLandOffset;
-      } else if (width >= 992) {
-        return colWidth * mdLandOffset;
-      } else if (width >= 768) {
-        return colWidth * smLandOffset;
-      } else {
-        return colWidth * xsLandOffset;
-      }
+  /// Method to return grid size name
+  static int gridSize(double width) {
+    var sizeName = xs;
+    if (width >= 576 && width < 768) {
+      sizeName = sm;
+    } else if (width >= 768 && width < 992) {
+      sizeName = md;
+    } else if (width >= 992 && width < 1200) {
+      sizeName = lg;
+    } else if (width >= 1200) {
+      sizeName = xl;
     }
+
+    return sizeName;
   }
 }
